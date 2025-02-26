1a7913ccd679541d4b297989dbceb210bf0a6e6e
Allan Reis <allanreisdev@outlook.com>
2025-02-26 00:58:17 -0300

    Adiciona variáveis e exibe informações no console
    Implementadas declarações e inicializações de variáveis de tipos `int`, `double`, `string` e `bool`. Mensagens foram adicionadas para exibir os valores dessas variáveis no console.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Declarando e inicializando variáveis de tipos diferentes
+
+
+int idade = 30;
+Console.WriteLine($"Idade: {idade}");
+
+double precoProduto = 129.99;
+Console.WriteLine($"Preço do produto: R${precoProduto}");
+
+string nomeUsuario = "Aline";
+Console.WriteLine($"Nome do usuario: {nomeUsuario}");
+
+bool usuarioLogado = true;
+Console.WriteLine($"Usuário estaa logado: {usuarioLogado} ");
